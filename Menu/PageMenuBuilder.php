66061c7aad07a10aba697df143e65683d3d7199b
Benjamin Seibert <benjamin.seibert@gmx.de>
2012-07-25 14:41:53 +0200

    made the page menu builder more flexible

--- Menu/PageMenuBuilder.php
+++ Menu/PageMenuBuilder.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Room13\PageBundle\Entity\Page;
 use Knp\Menu\ItemInterface;
 
+use Room13\TwigExtensionsBundle\Exception\InvalidArgumentException;
+
 class PageMenuBuilder
 {
     /**
@@ -21,27 +23,56 @@ class PageMenuBuilder
      */
     private $em;
 
+    private $defaultOptions = array(
+        'depth'=>1,
+        'title_property'=>'title',
+        'root_as_child'=>true,
+    );
+
     public function __construct(FactoryInterface $factory, EntityManager $em)
     {
         $this->factory = $factory;
         $this->em = $em;
     }
 
-    public function createPageMenu(Request $request, $paths, $depth)
+
+    public function createPageMenu(Request $request, $paths, array $options=array())
     {
 
-        if(!is_array($paths))
+        if(is_string($paths))
         {
+            // got a single string, marshal it as array
             $paths = array($paths);
         }
 
-        $menu = $this->factory->createItem('root');
-        $menu->setCurrentUri($request->getPathInfo());
+        if(!is_array($paths))
+        {
+            throw new InvalidArgumentException(
+                'Paths for menu pages invalid. Please specify an array of paths of a single path to build the menu from.'
+            );
+        }
+
+        $options = array_merge($this->defaultOptions,$options);
+
+
+        $menu = $this
+            ->factory->createItem('root')
+            ->setCurrentUri($request->getPathInfo())
+        ;
 
         foreach($paths as $path)
         {
             $page = $this->em->getRepository('Room13PageBundle:Page')->findOneByPath($path);
-            $this->buildMenu($menu,$page,$depth);
+
+            if($page === null)
+            {
+                throw new InvalidArgumentException(sprintf(
+                    'Menu page with path "%s" not found',
+                    $path
+                ));
+            }
+
+            $this->buildMenu($menu,$page,$options);
         }
 
 
@@ -50,14 +81,31 @@ class PageMenuBuilder
 
     }
 
-    private function buildMenu(ItemInterface $menu,Page $page,$depth)
+    private function buildMenu(ItemInterface $rootMenu,Page $page,array $options)
     {
-        $item = $menu->addChild($page->getTitle());
+        $titleGetter = 'get'.ucfirst($options['title_property']);
+
+        $menu = $rootMenu
+            ->addChild($page->$titleGetter())
+            ->setAttribute('id',$page->getId())
+        ;
+
+        if($options['root_as_child'])
+        {
+            $menu
+                ->addChild($page->$titleGetter())
+                ->setUri($page->getMainUrl())
+                ->setAttribute('id',$page->getId())
+            ;
+        }
 
         foreach($page->getChildren() as $childPage)
         {
-            $child = $item->addChild($childPage->getTitle());
-            $child->setUri($childPage->getMainUrl());
+            $menu
+                ->addChild($childPage->$titleGetter())
+                ->setUri($childPage->getMainUrl())
+                ->setAttribute('id',$childPage->getId())
+            ;
         }
 
 
